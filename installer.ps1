f146dc1b2a6975b5f548957aed74ffc4982567e1
Myles Mattlock <myles.mattlock@outlook.com>
2025-12-16 11:16:36 +0000

    fixed shortcut

--- installer.ps1
+++ installer.ps1
@@ -36,11 +36,10 @@ try {
     Write-Output "An error occurred while running the installer"
     Write-Output $_.Exception.Message
 }
-Copy-Item -Path "SystemUpdate" -Destination "C:\Program Files\SystemUpdate" -Recurse -Force
-#path to .exe file
+Copy-Item -Path "SystemUpdate\*" -Destination "C:\Program Files\SystemUpdate" -Recurse -Force
+
 $targetPath = "C:\Program Files\SystemUpdate\System Update.exe"
 
-#name for shortcut
 $shortcutName = "System Update"
 
 $desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
@@ -48,16 +47,10 @@ $shortcutPath = Join-Path $desktopPath "$shortcutName.lnk"
 $WshShell = New-Object -ComObject WScript.Shell
 $shortcut = $WshShell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $targetPath
-
-#Set a description for the shortcut (what appears as a tooltip)
 $shortcut.Description = "Update Windows using Myles' Tool"
-
 $shortcut.Save()
 
 Write-Host "Shortcut for '$shortcutName' created successfully on the desktop."
-
-
-
 
 
 Write-Output "Successfully installer Myles updater, closing..."
